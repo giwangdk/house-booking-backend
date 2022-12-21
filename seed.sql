ceb63b5e30f28b713ce5d5a40ae77da18bd5faac
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-21 17:33:20 +0700

    feat: seeding house_photo

--- seed.sql
+++ seed.sql
@@ -48,5 +48,19 @@ INSERT INTO houses VALUES
 11 )
 
 
-INSERT INTO houses_details VALUES
-()
+INSERT INTO houses_photos VALUES (
+
+)
+
+
+INSERT INTO house_photos VALUES
+(1,1, 'https://a0.muscache.com/im/pictures/e25a9b25-fa98-4160-bfd1-039287bf38b6.jpg?im_w=1200'),
+(2,1,'https://a0.muscache.com/im/pictures/miso/Hosting-34113796/original/f4f7b242-db33-46fc-9080-c3d6a6fd55ec.jpeg?im_w=1440'),
+(3,1,'https://a0.muscache.com/im/pictures/miso/Hosting-34113796/original/372e7d6f-7fb9-4668-92f0-25bb9a346814.jpeg?im_w=1440'),
+(4,1, 'https://a0.muscache.com/im/pictures/miso/Hosting-34113796/original/4756e699-f474-4ca7-8b77-06b12715a6cb.jpeg?im_w=1440'),
+(5,1,'https://a0.muscache.com/im/pictures/miso/Hosting-34113796/original/fca892a4-3481-4ad1-9f92-404feaa42e9f.jpeg?im_w=1440'),
+(6,1,'https://a0.muscache.com/im/pictures/miso/Hosting-34113796/original/36d8007a-0de5-439d-9fec-1c2d7b53a147.jpeg?im_w=1440'),
+(7,1, 'https://a0.muscache.com/im/pictures/miso/Hosting-34113796/original/f95b0a2e-0272-469e-a56c-433b9cc4ffdb.jpeg?im_w=1440'),
+(8,1, 'https://a0.muscache.com/im/pictures/miso/Hosting-34113796/original/97108083-280f-4e0e-9f1b-7a4b0dd34c44.jpeg?im_w=1440'),
+(9,1, 'https://a0.muscache.com/im/pictures/miso/Hosting-34113796/original/8a9f650e-be60-47a9-833f-2ed0950cb9f9.jpeg?im_w=1440'),
+(10,1, 'https://a0.muscache.com/im/pictures/miso/Hosting-34113796/original/6308de24-4c50-4bb6-8a66-ae42b45a2ac2.jpeg?im_w=1440')
