2d3e1d0d7019990b8e27d518259c7e2a0e5fd4eb
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2022-12-21 12:34:45 +0700

    fix: delete * in middleware isHost

--- middleware/isHost.go
+++ middleware/isHost.go
@@ -6,9 +6,10 @@ import (
 
 	"github.com/gin-gonic/gin"
 )
-func IsHost (c *gin.Context) {
+
+func IsHost(c *gin.Context) {
 	user, _ := c.Get("user")
-	if user.(*dto.UserJWT).Role != "host"{
+	if user.(dto.UserJWT).Role != "host" {
 		err := httperror.UnauthorizedError()
 		c.AbortWithStatusJSON(err.StatusCode, err)
 		return
