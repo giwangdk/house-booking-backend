298aa17ceee73233dfec7ac922c7a79cc28eb468
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2023-01-01 13:47:41 +0700

    feat: add middleware to for role admin and host

---
+++ middleware/isAdminAndHost.go
@@ -0,0 +1,21 @@
+package middleware
+
+import (
+	"final-project-backend/dto"
+	"final-project-backend/httperror"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IsAdminAndHost(c *gin.Context) {
+	user, _ := c.Get("user")
+	if user.(dto.UserJWT).Role != "admin" && user.(dto.UserJWT).Role != "host"{
+		err := httperror.UnauthorizedError()
+		c.AbortWithStatusJSON(err.StatusCode, err)
+		return
+	}
+
+	fmt.Println("role", user.(dto.UserJWT).Role )
+	c.Next()
+}
