13f669133d11ba1bcec0832c8ba808ecf7276de4
Giwang Dwi Kintan <giwang.kintan@shopee.com>
2023-01-03 23:30:10 +0700

    feat: add erd to readme

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ type AppConfig struct {
 
 func getENV(key, defaultVal string) string {
 
-	err := godotenv.Load("../.env")
+	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
