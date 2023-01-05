090518779c3d20d6f46e9ff676258f1e869f577f
Giwang Dwi Kintan <73090361+giwangdk@users.noreply.github.com>
2023-01-05 10:48:42 +0700

    debugging

--- middleware/authorize.go
+++ middleware/authorize.go
@@ -18,7 +18,7 @@ func validateToken(token string) (*jwt.Token, error) {
 		if _, isValid := t.Method.(*jwt.SigningMethodHMAC); !isValid {
 
 			//todo create sentinel error or http error
-			fmt.Println(token)
+			fmt.Println("hihhiihi",token)
 			return nil, errors.New("invalid token signature")
 		}
 		return []byte("very-secret"), nil
